1ac0c1886bf48c9b917dc02bfbeea99a5d444beb
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2023-03-02 11:19:56 +0530

    uploading js basics files

---
+++ JavaScript/javascript basics/basics.js
@@ -0,0 +1,80 @@
+    // document.getElementById("title").innerHTML = "Learning Html, CSS and Javascript"
+    // document.getElementById("title").style.color = "green"
+    // document.getElementById("title").style.backgroundColor = "red"
+
+    // document.getElementById("content").style.fontSize = "20px"
+
+
+    // console.log("sadfkasjdf");
+    // console.log(2+4-343)
+
+    // alert("hello javaScript sdsds")
+
+    // var feeling = window.prompt("are you happy today ?")
+    // console.log(feeling)
+
+    // document.getElementById("content").innerHTML = feeling
+    // window.confirm("fasdlfsd")
+
+    // document.write("helo sdfsdafa")
+
+    // window.print()
+
+    // var laptopName = window.prompt("what is your laptop name")
+    // console.log(laptopName)
+
+    // document.getElementById("laptop").innerHTML = laptopName
+
+    // var price = window.prompt("price of the laptop")
+    // console.log( typeof price)
+    // document.getElementById("price").innerHTML = price
+
+    // var a = window.prompt("enter value of a");
+    // var b = window.prompt("enter value of b");
+    // var c = a+b;
+    // console.log(c)
+    // console.log(typeof c)
+
+    // var a = 10;
+    // var b = 20;
+    // var c = a+b
+    // console.log(c)
+    // console.log(typeof a, typeof b, typeof c)
+
+
+    // var d = "learning "
+    // var e = "Javascript"
+    // var f = d + e
+    // console.log(f)
+    // console.log(typeof d, typeof e, typeof f)
+
+    // var day;
+    // console.log("today is" + " " + day)
+    // console.log(typeof day)
+
+    // var fiveApplesCost = "fifty";
+    // var oneAppleCost = 10; var tenApplesCost = fiveApplesCost * 2;  console.log(tenApplesCost);
+    // console.log(typeof tenApplesCost)
+
+    var courseName = "frontend"
+    var Duration = "2 months"
+    var IMPORTANT_PROJECT = "NETFLIX CLONE"
+    var project1 = "simple form"
+    var $total_projects = 10;
+    var _tools = ["vsCode","github","netlify"]
+    var syllabus = "asdlfkk"
+
+    // var 2project = "simple html table"
+    // var *project = "Flipkar clone"
+
+    var courseOverView = "we are learning" + " " 
+                        + courseName + " " + "course"
+                        + ", The Duration of the course is" + " " + Duration
+    console.log(courseOverView)
+
+    var totalPrice;
+    console.log(totalPrice)
+
+    totalPrice = totalPrice + 100
+    console.log(totalPrice)
+    
